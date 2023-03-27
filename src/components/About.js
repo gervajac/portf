b31223b3c7834dcf6b81fc5be210afc2adee918e
gervajac <gerva.jacob.tropini@gmail.com>
2023-03-27 15:04:13 -0300

    fixed

--- src/components/About.js
+++ src/components/About.js
@@ -1,18 +1,16 @@
 import React from "react";
 import CountUp from "react-countup";
-import img from "../assets/gerva8.jpg";
+
 
 const About = () => {
   return (
     <section className="section" id="about">
       <div className="container mx-auto">
-        <div className="flex flex-col gap-y-10 lg:flex-row lg:items-center lg:gap-x-20 lg:gap-y-0 h-[400px]">
+        <div className="flex flex-col gap-y-10 lg:flex-row lg:items-center lg:gap-x-20 lg:gap-y-0 h-screen">
           {/* img */}
-          <div className="flex-1  bg-contain bg-no-repeat max-h-auto mix-blend-lighten bg-top">
-            <img src={img} alt="" className="rounded-full brightness-80"></img>
-          </div>
-          {/* text */}
-          <div className="flex-1">
+          <div className="flex-1 bg-about bg-contain bg-no-repeat h-[640px] mix-blend-lighten bg-top rounded-full brightness-80"></div>
+            {/* text */}
+            <div className="flex-1">
             <h2 className="h2 text-accent">About Me.</h2>
             <h3 className="h3 mb-4">
               I'm a Freelance Full Stack developer with more than 800 hours in a
@@ -25,29 +23,27 @@ const About = () => {
               vocation.
             </p>
             {/* stats */}
-            <div className="flex gap-x-6 lg:pag-x-10 mb-12">
-              <div className="text-[40px] font-tertiary text-gradient mb-2">
-                <CountUp start={0} end={2} duration={3} />
-              </div>
-              <div className="font-primary text-sm tracking-[2px]">
-                Years of 
-                Experience
-              </div>
+          <div className="flex gap-x-6 lg:pag-x-10 mb-12">
+            <div className="text-[40px] font-tertiary text-gradient mb-2">
+              <CountUp start={0} end={2} duration={3} />
             </div>
-            <div className="flex gap-x-6 lg:pag-x-10 mb-12">
-              <div className="text-[40px] font-tertiary text-gradient mb-2">
-                +<CountUp start={0} end={10} duration={2} />
-              </div>
-              <div className="font-primary text-sm tracking-[2px]">
-                  Proyects
-              </div>
+            <div className="font-primary text-sm tracking-[2px]">
+              Years of <br />
+              Experience
             </div>
-            <div className="flex gap-x-8 items-center">
-              <button className="btn btn-lg">Contact me</button>
-              <a href="#" className="text-gradient btn-link">
-                My Portfolio
-              </a>
+          </div>
+          <div className="flex">
+            <div className="text-[40px] font-tertiary text-gradient mb-2">
+              <CountUp start={0} end={5} duration={3} />
             </div>
+            <div className="font-primary text-sm tracking-[2px]">Proyects</div>
+          </div>
+          <div className="flex gap-x-8 items-center">
+            <button className="btn btn-lg">Contact me</button>
+            <a href="#" className="text-gradient btn-link">
+              My Portfolio
+            </a>
+          </div>
           </div>
         </div>
       </div>
